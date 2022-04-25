e3c51c60dd197133ef25b1eddcd37adbf9d2b40c
db92-coder <80090474+db92-coder@users.noreply.github.com>
2022-04-25 17:18:51 +1000

    Add project files.

---
+++ HRIS/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace HRIS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //if()
+            //{
+            //    MainWindow mainWindow = new MainWindow();
+            //    mainWindow.Show();
+            //}
+            //else
+            //{
+            //    LoginWindow otherWindow = new LoginWindow();
+            //    otherWindow.Show();
+            //}
+        }
+    }
+}
---
+++ HRIS/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HRIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //private const string STAFF_KEY = "viewableStaff";
+        //private StaffController staffController;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    DragMove();
+            }
+        }
+
+        private void ButtonMinimise_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        //private void DisplayLoginScreen()
+        //{
+        //    LoginWindow frm = new LoginWindow();
+
+        //    frm.Owner = this;
+        //    frm.ShowDialog();
+        //    if (frm.DialogResult.HasValue && frm.DialogResult.Value)
+        //        MessageBox.Show("User Logged In");
+        //    else
+        //        this.Close();
+        //}
+    }
+}
+
